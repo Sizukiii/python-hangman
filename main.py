f7f2d42dc45a34ac5ce86e3b79e34fa07853fa6d
John Ludlow <gamerguy0570@gmail.com>
2022-07-11 02:40:10 -0700

    Added more functions to the game

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
 
-chosen_word = random.choice(word_list);
-print(chosen_word)
-guess = input("guess a letter ").lower()
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-for letter in chosen_word:
- if letter == guess:
-    print("yes")
-else: 
-    print("false")
+
+display = [];
+length = len(chosen_word)
+for _ in range(length):
+  display += "_";
+print(display);
+
+guess = input("Guess a letter: ").lower()
+
+
+for position in range(length):
+   letter = chosen_word[position]
+   if letter == guess:
+     display[position] = letter
+  
+
+
+print(display)
